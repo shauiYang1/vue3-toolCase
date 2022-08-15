f84ee40821c9f6c7548480188130be7b11a1c545
yangshuai <u9h_75bugk181@dingtalk.com>
2022-08-15 15:47:53 +0800

    全局属性设置测试

--- src/main.ts
+++ src/main.ts
@@ -2,17 +2,38 @@
  * @Author: yangshuai u9h_75bugk181@dingtalk.com
  * @Date: 2022-06-15 17:07:21
  * @LastEditors: yangshuai u9h_75bugk181@dingtalk.com
- * @LastEditTime: 2022-06-30 14:38:50
+ * @LastEditTime: 2022-08-15 15:47:18
  * @FilePath: \vue3-test\src\main.ts
  * @Description: 项目入口文件
  */
-import { createApp } from 'vue'
-import ElementPlus from 'element-plus'
-import App from './App.vue'
-import router from './router'
-import 'lib-flexible'
-import 'element-plus/dist/index.css'
-import '@/assets/styles/pubilc.scss'
-import '@/assets/styles/common.scss'
+import { createApp } from "vue";
+import ElementPlus from "element-plus";
+import App from "./App.vue";
+import router from "./router";
+import "lib-flexible";
+import "element-plus/dist/index.css";
+import "@/assets/styles/pubilc.scss";
+import "@/assets/styles/common.scss";
 
-createApp(App).use(ElementPlus).use(router).mount('#app')
+const MyPlugin = {
+  install(app, options) {
+    console.log(app, options); // 打印参数
+    // 1. 添加全局方法或属性
+    app.config.globalProperties.$myMethod = (options) => {
+      // 逻辑...
+      return options;
+    };
+
+    // 2. 添加全局资源指令
+    app.directive("my-directive", {
+      bind(el, binding, vnode, oldVnode) {
+        // 逻辑...
+      },
+    });
+  },
+};
+createApp(App)
+  .use(ElementPlus)
+  .use(router)
+  .use(MyPlugin, { testName: "张三" })
+  .mount("#app");
--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -2,19 +2,18 @@
  * @Author: yangshuai u9h_75bugk181@dingtalk.com
  * @Date: 2022-06-16 10:41:52
  * @LastEditors: yangshuai u9h_75bugk181@dingtalk.com
- * @LastEditTime: 2022-08-04 10:20:38
+ * @LastEditTime: 2022-08-12 16:13:27
  * @FilePath: \vue3-test\src\pages\systems\index.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
-<template>
-  <div class="Home">Home</div>
-</template>
-<script>
-import { defineComponent } from "@vue/composition-api";
 
-export default defineComponent({
-  name: "Home",
-  setup() {},
+<template>
+  <div class="Home">{{ $myMethod() }}</div>
+</template>
+<script setup>
+import { onMounted } from "vue";
+onMounted(() => {
+  console.log("this.$myMethod");
 });
 </script>
 
