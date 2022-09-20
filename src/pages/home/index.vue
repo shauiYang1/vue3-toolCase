c131f9744e23d4011167b6d8509d38c1ffcc249d
yangshuai <u9h_75bugk181@dingtalk.com>
2022-09-20 09:37:04 +0800

    新增测试路由页面,增加可编辑p标签演示

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -2,13 +2,16 @@
  * @Author: yangshuai u9h_75bugk181@dingtalk.com
  * @Date: 2022-06-16 10:41:52
  * @LastEditors: yangshuai u9h_75bugk181@dingtalk.com
- * @LastEditTime: 2022-08-12 16:13:27
+ * @LastEditTime: 2022-09-20 09:35:32
  * @FilePath: \vue3-test\src\pages\systems\index.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
 
 <template>
-  <div class="Home">{{ $myMethod() }}</div>
+  <div class="Home">
+    <div><label>可编辑的p标签</label><p contenteditable="true">12</p></div>
+    
+  </div>
 </template>
 <script setup>
 import { onMounted } from "vue";
@@ -16,4 +19,3 @@ onMounted(() => {
   console.log("this.$myMethod");
 });
 </script>
-
--- src/router/index.ts
+++ src/router/index.ts
@@ -2,47 +2,54 @@
  * @Author: yangshuai u9h_75bugk181@dingtalk.com
  * @Date: 2022-06-16 10:40:38
  * @LastEditors: yangshuai u9h_75bugk181@dingtalk.com
- * @LastEditTime: 2022-08-12 10:54:23
+ * @LastEditTime: 2022-09-16 15:53:57
  * @FilePath: \vue3-test\src\router\index.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
-import {
-    createRouter,
-    createWebHistory,
-    RouteRecordRaw
-} from 'vue-router';
-import Home from '@/pages/home/index.vue';
-import FFmpeg from '@/pages/ffmpeg/index.vue';
-import Style from '@/pages/style/index.vue'
-import MixBlendMode from '@/pages/style/mixBlendMode.vue'
-import MQTT from '@/pages/mqtt/index.vue'
+import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
+import Home from "@/pages/home/index.vue";
+import FFmpeg from "@/pages/ffmpeg/index.vue";
+import Style from "@/pages/style/index.vue";
+import MixBlendMode from "@/pages/style/mixBlendMode.vue";
+import MQTT from "@/pages/mqtt/index.vue";
+import TSDemo from "@/pages/tsdemo/index.vue";
 
-export const routes: Array<RouteRecordRaw> = [{
-    path: '/',
-    name: '首页',
-    component: Home
-}, {
-    path: '/ffmpeg',
-    name: '素材裁剪',
-    component: FFmpeg
-}, {
-    path: '/style',
-    name: '动画演示',
+export const routes: Array<RouteRecordRaw> = [
+  {
+    path: "/",
+    name: "首页",
+    component: Home,
+  },
+  {
+    path: "/ffmpeg",
+    name: "素材裁剪",
+    component: FFmpeg,
+  },
+  {
+    path: "/style",
+    name: "动画演示",
     component: Style,
     children: [
-        {
-            path: '/style/mixBlendMode',
-            name: 'css混合模式',
-            component: MixBlendMode
-        }
-    ]
-}, {
-    path: '/mqtt',
-    name: 'mqtt',
-    component: MQTT
-}];
+      {
+        path: "/style/mixBlendMode",
+        name: "css混合模式",
+        component: MixBlendMode,
+      },
+    ],
+  },
+  {
+    path: "/mqtt",
+    name: "mqtt",
+    component: MQTT,
+  },
+  {
+    path: "/ts",
+    name: "ts",
+    component: TSDemo,
+  },
+];
 const router = createRouter({
-    history: createWebHistory(process.env.BASE_URL),
-    routes
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
 });
 export default router;
